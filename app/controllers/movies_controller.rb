9e19c8fdfadffa4279532d4b8243d564996796b1
Aotokitsuruya <contact@aotoki.me>
2023-05-28 14:32:51 +0800

    Add movielist filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
+  def index
+    @movies = [
+      'SANKYO PRESENTS WALKURE FINAL LIVE TOUR 現場直播',
+      '小美人魚',
+      '玩命關頭X'
+    ]
+
+    @movies = ['小美人魚'] if params[:search] == '最近 1'
+  end
 end
